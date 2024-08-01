438cf04107b385f914573985c28565bd927e3e55
ayossa <ayusha.kanade.sod23@anu.edu.in>
2024-08-01 17:18:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(640, 640);
+
+  background(200);
+
+  circle(320, 320, 160);
+
+  describe('A white circle with black outline in the middle of a gray canvas.');
+}
